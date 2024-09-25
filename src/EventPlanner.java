56c2afd6e4239eb7602ada5a19c24b18bdf31c91
Hadley Bennett <hbennett2@cub.uca.edu>
2024-09-24 22:56:31 -0500

    added comments

--- src/EventPlanner.java
+++ src/EventPlanner.java
@@ -1,16 +1,18 @@
+// Event planner -- Add defaults
+
 import java.awt.*;
 import javax.swing.*;
 import java.time.LocalDateTime;
 
-public class EventPlanner {
-
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Event Planner");
+public class EventPlanner
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("EVENT PLANNER"); // title GUI
+        frame.setPreferredSize(new Dimension(700, 600)); // size window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(800, 600));
 
-        EventListPanel eventlistpanel = new EventListPanel();
+        EventListPanel eventlistpanel = new EventListPanel(); // instantiate
         addDefaultEvents(eventlistpanel);
         frame.add(eventlistpanel);
         frame.pack();
@@ -18,18 +20,15 @@ public class EventPlanner {
     }
 
     // add defaults events
-    public static void addDefaultEvents(EventListPanel eventListPanel) {
-
-        LocalDateTime deadlineDateTime = LocalDateTime.of(2024, 9, 22, 15, 0);
-        Deadline deadline = new Deadline("Submit Project", deadlineDateTime);
-
-
-        LocalDateTime meetingStartDateTime = LocalDateTime.of(2024, 9, 23, 15, 0);
-        LocalDateTime meetingEndDateTime = LocalDateTime.of(2024, 9, 23, 16, 0);
-        Meeting meeting = new Meeting("Touchbase Meeting", meetingStartDateTime, meetingEndDateTime, "Conference Room 1");
-
-
-        eventListPanel.addEvent(deadline);
+    public static void addDefaultEvents(EventListPanel eventListPanel)
+    {
+        LocalDateTime mStartDate = LocalDateTime.of(2024, 9, 27, 13, 0);
+        LocalDateTime mEndDate = LocalDateTime.of(2024, 9, 27, 14, 0);
+        Meeting meeting = new Meeting("Overview Conference", mStartDate, mEndDate, "Room 335");
         eventListPanel.addEvent(meeting);
+
+        LocalDateTime dDate = LocalDateTime.of(2024, 12, 30, 12, 0);
+        Deadline deadline = new Deadline("Finish Lab", dDate);
+        eventListPanel.addEvent(deadline);
     }
 }
